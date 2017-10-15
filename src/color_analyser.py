ad22071b653fa8a54e9cdbe85c45f937a0f36077
Lucas Maurice <lucas.maurice@outlook.com>
2017-10-14 20:38:41 -0400

    write a method for give the color of a 2d array of pixels.

--- src/color_analyser.py
+++ src/color_analyser.py
@@ -1,108 +1,112 @@
 #!/usr/bin/env python
 
+# Values for colors :
+# 0    black
+# 1    grey
+# 2    white
+# 3    red
+# 4    yellow
+# 5    green
+# 6    cyan
+# 7    blue
+# 8    magenta
+
 # Imports for ROS
 # import rospy
-# from std_msgs.msg import String
 
-import colorsys
+from colorsys import rgb_to_hsv
 from PIL import Image
 
-def rgb_to_hsv(color):
-    r = color[0]
-    g = color[1]
-    b = color[2]
+# Declaration of constants
+nbr_of_colors = 9
 
-    # Read the Higher and the Lower color
-    mini = min(r, g, b)
-    maxi = max(r, g, b)
+# Colors array
+color_name = [""] * nbr_of_colors
+color_name[0] = "black"
+color_name[1] = "grey"
+color_name[2] = "white"
+color_name[3] = "red"
+color_name[4] = "yellow"
+color_name[5] = "green"
+color_name[6] = "cyan"
+color_name[7] = "blue"
+color_name[8] = "magenta"
 
-    # Calculate the difference between Higher and Lower
-    delta = maxi - mini
 
-    # The Value parameter correspond to the higher color intensity
-    v = maxi
+def _rgb_to_hsv(_color):
+    r = _color[0]
+    g = _color[1]
+    b = _color[2]
 
-    # if the differences between higher and lower color is too short, the color is monochromatic
-    # so saturation is null and hue is undefined (we set 0 for never crash the application)
-    if delta == 0:
-        s = 0
-        h = 0
-        return [h, s, v]
+    h, s, v = rgb_to_hsv(r / 255., g / 255., b / 255.)
 
-    # if the maximum value is null, then the color will be black.
-    # we set 0 again for never crash the application
-    if maxi == 0:
-        s = 0
-        h = 0
-        return [h, s, v]
-
-    s = delta / maxi * 255
-
-    # calculate the hue of the selected color
-    if r >= maxi:
-        h = (g - b) / delta
-
-    elif g >= maxi:
-        h = (b - r) / delta + 510
-
-    else:
-        h = (r - b) / delta + 1020
-
-    h *= 0.23529411764705882
-
-    if h < 0:
-        h += 360
+    h *= 360.
+    s *= 100.
+    v *= 100.
 
     # Return the result
     return [h, s, v]
 
 
-def get_color_ID(rgb):
-    hue, saturation, value = rgb_to_hsv(rgb)
+def get_color_id(_rgb):
+    hue, saturation, value = _rgb_to_hsv(_rgb)
 
     # Assume it is black
-    if value < 20:
-        return 'white'
+    if value < 15:
+        return 0
 
     # Assume it is monochrome
     if saturation < 20:
 
-        # Assume it is black
-        if value > 235:
-            return 'black'
+        # Assume it is white
+        if value > 90:
+            return 2
 
         # Assume it is grey
         else:
-            return 'grey'
+            return 1
 
     # Assume it is chromatic
     else:
 
         # For each colors
-        if 0 < hue <= 30:
-            return 'red'
+        if 0 <= hue <= 30:
+            return 3
         if 30 < hue <= 90:
-            return 'yellow'
+            return 4
         if 90 < hue <= 150:
-            return 'green'
+            return 5
         if 150 < hue <= 210:
-            return 'cyan'
+            return 6
         if 210 < hue <= 270:
-            return 'blue'
+            return 7
         if 270 < hue <= 330:
-            return 'magenta'
+            return 8
         if 330 < hue <= 360:
-            return 'red'
+            return 3
+
+
+def get_mean_color(_pixels, _width, _height):
+    color_list = [0] * nbr_of_colors
+    for x_position in range(0, _width):
+        for y_position in range(0, _height):
+            _color_id = get_color_id(_pixels[x_position, y_position])
+            color_list[_color_id] += 1
+
+    nbr_of_pixels = _width * _height / 100
+    for _color_id in range(0, nbr_of_colors):
+        color_list[_color_id] = color_list[_color_id] / nbr_of_pixels
+
+    return color_list
 
 
 # Read image
-im = Image.open('red.jpg')
+im = Image.open('red.png')
 pix = im.load()
-
 width, height = im.size
 
-for x_position in range(0, width):
-    for y_position in range(0, height):
+color_list = get_mean_color(pix, width, height)
 
-        print(pix[x_position, y_position])
-        print(rgb_to_hsv(pix[x_position, y_position]))
+for color_id in range(0, nbr_of_colors):
+    print( "Color " + color_name[color_id] + ":  \t" + str(color_list[color_id]) + "%")
+
