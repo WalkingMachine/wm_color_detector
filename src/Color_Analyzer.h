4bebde00af9294b24a3d0ebf0c64208b0ae154a5
Lucas Maurice <lucas.maurice@outlook.com>
2018-03-12 13:31:43 -0400

    Add namespace for security with global variables.

--- src/Color_Analyzer.h
+++ src/Color_Analyzer.h
@@ -15,8 +15,6 @@
 #include <sstream>
 #include <string.h>
 
-
-
 //-----------------------------------------------------------------------
 //------------------------------- Colors --------------------------------
 //-----------------------------------------------------------------------
@@ -39,18 +37,19 @@ enum T_COLOR{
 //------------------------------ Functions ------------------------------
 //-----------------------------------------------------------------------
 
-/**
- * Print the version of OpenCV
- */
-void PrintVersion();
 
-std::string LoadAnImageFromCVMat(cv::Mat image, int x1, int x2, int y1, int y2);
 
-T_COLOR getColorID(int hue, int saturation, int value);
+namespace wm_color_analyser{
+	void PrintVersion();
 
-std::string getColorName(int color);
+	std::string LoadAnImageFromCVMat(cv::Mat image, int x1, int x2, int y1, int y2);
 
-void getColorValue(int colors[NBR_OF_COLOR], cv::Mat image_hsv, int x1, int x2, int y1, int y2);
+	T_COLOR getColorID(int hue, int saturation, int value);
+
+	std::string getColorName(int color);
+
+	void getColorValue(int colors[NBR_OF_COLOR], cv::Mat image_hsv, int x1, int x2, int y1, int y2);
+}
 
 #endif //PROJECT_COLOR_ANALYZER_H
 
--- src/ColorationAnalyser.h
+++ src/ColorationAnalyser.h
@@ -15,4 +15,10 @@
 #include "wm_color_detector/AnalyseColor.h"
 #include "wm_color_detector/BoundingBox.h"
 
+
+namespace wm_color_analyser{
+	bool get_bounding_boxes_color(wm_color_detector::AnalyseColor::Request &req, wm_color_detector::AnalyseColor::Response &res);
+	int init(int argc, char **argv);
+}
+
 #endif //PROJECT_TEST_H
